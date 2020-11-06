521459e653f418e1b95de713aeed72d319b0405b
ThomasWWebb <thomaswebb98uk@gmail.com>
2020-11-06 17:09:36 +0000

    Corrects the path to test data for opixray

--- configs/_base_/datasets/opixray_detection.py
+++ configs/_base_/datasets/opixray_detection.py
@@ -42,7 +42,7 @@ data = dict(
         pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/instances_val2017.json',
+        ann_file=data_root + 'annotations/instances_test2017.json',
         img_prefix=data_root + 'val2017/',
         pipeline=test_pipeline))
 evaluation = dict(interval=1, metric='bbox')
