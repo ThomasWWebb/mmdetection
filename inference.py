956bbf1bf9486b3be7d6b208a0715c88b52fbb7d
ThomasWWebb <thomaswebb98uk@gmail.com>
2021-01-05 11:39:13 +0000

    Corrects test set path

--- inference.py
+++ inference.py
@@ -179,7 +179,8 @@ def main():
 
     WINDOW_NAME = 'Detection'
     CLASSES = []
-    with open(test_set_path = test_set_root + "annotations/instances_test2017.json") as f:
+    test_set_path = test_set_root + "annotations/instances_test2017.json"
+    with open(test_set_path) as f:
        json_data = json.load(f)
     for data_id, data_info in json_data.items():
         if data_id == 'categories':
