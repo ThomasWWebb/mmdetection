75a7106bccf39fbfb6ee5cb2b6a8a3cfae9845fc
ThomasWWebb <thomaswebb98uk@gmail.com>
2021-01-12 23:40:49 +0000

    Prints detection scores for FA SGD on opixray

--- inference.py
+++ inference.py
@@ -176,7 +176,7 @@ def main():
     args = parser.parse_args()
     print(f'\n\n{args}\n\n')
 
-    models = ["free_anchor", "cascade_rcnn"]
+    models = ["free_anchor"]
     backbones = ["r50"]
     datasets = ["opixray"]
     # model_name = "cascade_rcnn"
@@ -190,9 +190,9 @@ def main():
                     config_file = './configs/{}/{}_{}_fpn_1x_coco_{}.py'.format(model_name,model_name, backbone, dataset)
                     if model_name == "free_anchor":
                         config_file = './configs/{}/retinanet_{}_{}_fpn_1x_coco_{}.py'.format(model_name,model_name, backbone, dataset)
-                    checkpoint_file = "./{}_{}_e30_{}_trueResolution_adam/epoch_10.pth".format(model_name, backbone, dataset)
+                    checkpoint_file = "./{}_{}_e30_{}_trueResolution/epoch_10.pth".format(model_name, backbone, dataset)
                     test_set_root = "../datasets/{}_subset".format(dataset)
-                    output_dir = "./{}_{}_e30_{}_trueResolution_adam/test_detect_subset_images".format(model_name, backbone, dataset)
+                    output_dir = "./{}_{}_e30_{}_trueResolution/test_detect_subset_images".format(model_name, backbone, dataset)
 
                     WINDOW_NAME = 'Detection'
                     CLASSES = []
