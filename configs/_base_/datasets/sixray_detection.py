0ea55449d4242edb2cb8ae41279fecefd6296077
ThomasWWebb <thomaswebb98uk@gmail.com>
2021-01-23 11:42:19 +0000

    Removes demand for mask gt in sixray val pipeline

--- configs/_base_/datasets/sixray_detection.py
+++ configs/_base_/datasets/sixray_detection.py
@@ -15,13 +15,13 @@ train_pipeline = [
 ]
 val_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+    dict(type='LoadAnnotations', with_bbox=True),
     dict(type='Resize', img_scale=(541, 440), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.0),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
 ]
 
 test_pipeline = [
