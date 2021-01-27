5f2163d11cb5e3ec34e3071e04286439a3161b41
ThomasWWebb <thomaswebb98uk@gmail.com>
2021-01-27 18:29:00 +0000

    Adds equalize transform to sixray

--- configs/_base_/datasets/sixray_detection.py
+++ configs/_base_/datasets/sixray_detection.py
@@ -2,13 +2,20 @@ dataset_type = 'SIXrayDataset'
 data_root = 'datasets/sixray/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+
+albu_transforms = [
+    dict(type='Equalize',
+    mode='cv',
+    by_channels=False)]
+
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
     dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
     #dict(type='RandomFlip', flip_ratio=0.75, direction=['horizontal', 'vertical']),
     dict(type='RandomFlip', flip_ratio=0.0),
-    dict(type='custom_RandomCrop',crop_type='relative_range', crop_size=(0.75, 0.75)),
+    dict(type='Albu', transforms=albu_transforms),
+    #dict(type='custom_RandomCrop',crop_type='relative_range', crop_size=(0.75, 0.75)),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
