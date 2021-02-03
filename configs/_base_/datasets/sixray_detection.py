a4f50047c51c3c270a9caec33ec4fc6d30f3a688
ThomasWWebb <thomaswebb98uk@gmail.com>
2021-02-03 11:24:01 +0000

    Actually adds blur to sixray

--- configs/_base_/datasets/sixray_detection.py
+++ configs/_base_/datasets/sixray_detection.py
@@ -4,7 +4,7 @@ img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 
 albu_transforms = [
-    # dict(type='Equalize',
+    #dict(type='Equalize',
     # mode='cv',
     # by_channels=False)]
     dict(type='Blur')]
@@ -16,7 +16,7 @@ train_pipeline = [
     dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
     #dict(type='RandomFlip', flip_ratio=0.75, direction=['horizontal', 'vertical']),
     dict(type='RandomFlip', flip_ratio=0.0),
-    #dict(type='Albu', transforms=albu_transforms),
+    dict(type='Albu', transforms=albu_transforms),
     #dict(type='custom_RandomCrop',crop_type='relative_range', crop_size=(0.75, 0.75)),
     #dict(type='Rotate',level=10),
     dict(type='Normalize', **img_norm_cfg),
