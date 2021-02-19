4c82947c12d55974c44928512f360beaa251909c
ThomasWWebb <thomaswebb98uk@gmail.com>
2021-02-19 11:18:52 +0000

    Testing bboxmixup in the sixray pipeline

--- configs/_base_/datasets/sixray_detection.py
+++ configs/_base_/datasets/sixray_detection.py
@@ -7,15 +7,16 @@ albu_transforms = [
     #dict(type='Equalize',
     #mode='cv',
     #by_channels=False)]
-    dict(type='Blur')]
+    #dict(type='Blur')]
     #dict(type='JpegCompression', quality_lower=10, quality_upper=11)]
 
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='custom_MixUp', mixUp_prob=0.5),
+    #dict(type='custom_MixUp', mixUp_prob=0.5),
+    dict(type='custom_bboxMixUp', mixUp_prob=0.5),
     dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
-    #dict(type='RandomFlip', flip_ratio=0.75, direction=['horizontal', 'vertical']),
+    #dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='RandomFlip', flip_ratio=0.0),
     #dict(type='Albu', transforms=albu_transforms),
     #dict(type='custom_RandomCrop',crop_type='relative_range', crop_size=(0.75, 0.75)),
