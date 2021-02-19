89f995409a93e176d8c72f2e32d624e17e985a82
ThomasWWebb <thomaswebb98uk@gmail.com>
2021-02-19 11:57:22 +0000

    Corrects sixray pipeline formatting

--- configs/_base_/datasets/sixray_detection.py
+++ configs/_base_/datasets/sixray_detection.py
@@ -4,33 +4,24 @@ img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 
 albu_transforms = [
-    #dict(type='Equalize',
-    #mode='cv',
-    #by_channels=False)]
-    #dict(type='Blur')]
-    #dict(type='JpegCompression', quality_lower=10, quality_upper=11)]
+    #dict(type='Equalize',mode='cv',by_channels=False)
+
+    #dict(type='Blur')
+
+    #dict(type='JpegCompression', quality_lower=10, quality_upper=11)
+    ]
 
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
     #dict(type='custom_MixUp', mixUp_prob=0.5),
-    dict(type='custom_bboxMixUp', mixUp_prob=0.5),
+    #dict(type='custom_bboxMixUp', mixUp_prob=0.5),
     dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
-    #dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='RandomFlip', flip_ratio=0.0),
+    #dict(type='RandomFlip', flip_ratio=0.5),
     #dict(type='Albu', transforms=albu_transforms),
     #dict(type='custom_RandomCrop',crop_type='relative_range', crop_size=(0.75, 0.75)),
     #dict(type='Rotate',level=10),
-    dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size_divisor=32),
-    dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
-]
-val_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
-    dict(type='RandomFlip', flip_ratio=0.0),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
