c4a43f5c45b14147a7bc2efc0f33713e4f9764ef
ThomasWWebb <thomaswebb98uk@gmail.com>
2021-03-13 16:26:42 +0000

    Applies cutmix to only the weakest opixray classes

--- configs/_base_/datasets/opixray_detection.py
+++ configs/_base_/datasets/opixray_detection.py
@@ -14,7 +14,7 @@ train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
     #dict(type='custom_bboxMixUp', mixUp_prob=0.5),
-    dict(type='custom_CutMix', cutMix_prob=0.5),
+    dict(type='custom_CutMix', cutMix_prob=0.5, class_targets={1:3, 3:1}),
     #dict(type='custom_RandomCrop',crop_type='relative_range', crop_size=(0.75, 0.75)),
     dict(type='Resize', img_scale=(1225, 954), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.0),
